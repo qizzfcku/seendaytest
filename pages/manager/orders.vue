38ea3739713668c34265e1b37bfda25ba19b630d
Dima Roschin <ardi.2005myros@gmail.com>
2024-05-17 16:54:52 +0700

    result of test task

---
+++ pages/manager/orders.vue
@@ -0,0 +1,239 @@
+<template>
+  <main>
+    <fixed-left-column>
+      <template v-slot:fixed>
+        <Button class="my-element">
+          Назад
+        </Button>
+        <div class="main_div block">
+          <div class="block">
+            <InputDate
+              v-model="selectedDate"
+              label="Период"
+              :range="true"
+              :required="true"
+              :with-checkmark="true"
+              @blur="handleBlur"
+            />
+          </div>
+          <div class="block">
+            <Search
+              v-model:searchQuery="searchQuery"
+              v-model:searchType="searchType"
+              :search-types="searchTypes"
+              select-display-value="icon"
+              @submit="handleSubmit"
+            />
+          </div>
+          <div class="toggle_block">
+            <toggle
+              v-for="item in toggleList"
+              :key="item.id"
+              v-model:pressed="item.isToggled.value"
+              class="toggle_block_element"
+              color="gray"
+              text-orientation="center"
+              width="inline"
+            >
+                <div class="toggle_block_element">{{item.text}}</div>
+            </toggle>
+          </div>
+        </div>
+        <div class="main_div block">
+          <Select
+            v-model="selectedValue"
+            label="Год"
+            :items="selectItems"
+            :editable="true"
+          />
+          <div class="bttns_element">
+            <Button
+              size="l"
+              @click="ShowButton"
+            >
+              Показать
+            </Button>
+            <Button
+              size="l"
+              color="gray-outline"
+              @click="ResetButton"
+            >
+              Сбросить
+            </Button>
+          </div>
+        </div>
+      </template>
+      <template v-slot:default>
+        <div v-if="!isLoading">
+          <StatusCard
+            v-for="item in items"
+            :key="item.id"
+            status="success"
+            class="status-card"
+          >
+            №: {{ item.id }}
+          </StatusCard>
+        </div>
+        <div v-else>
+          <Empty
+            :without-image="!isLoading"
+          >Заказов еще нет!
+          </Empty>
+        </div>
+      </template>
+    </fixed-left-column>
+  </main>
+</template>
+<script setup lang="ts">
+import { FixedLeftColumn } from "~/shared/ui/templates";
+import { InputDate } from "~/shared/ui/inputs/input-date";
+import { Search } from "~/shared/ui/search";
+import { Toggle } from "~/shared/ui/toggle";
+import { Select } from "~/shared/ui/select";
+import { StatusCard } from "~/shared/ui/status-card";
+import { Empty } from "~/shared/ui/empty";
+import {Button } from "~/shared/ui/button";
+
+
+const selectedDate = ref('');
+
+const searchQuery = ref('');
+const searchType = ref('order_number');
+
+const selectedValue = ref('');
+
+const items = ref([]);
+const isLoading = ref(false);
+const error = ref(null);
+
+const id = ref('');
+
+const toggleList = [
+  { id: 1, text: 'Все', isToggled: ref(false) },
+  { id: 2, text: '2', isToggled: ref(false) },
+  { id: 3, text: '3', isToggled: ref(false) },
+  { id: 4, text: '4', isToggled: ref(false) },
+  { id: 5, text: '5', isToggled: ref(false) },
+  { id: 6, text: '6', isToggled: ref(false) },
+  { id: 7, text: '7', isToggled: ref(false) },
+  { id: 8, text: '8', isToggled: ref(false) },
+  { id: 9, text: '9', isToggled: ref(false) }
+];
+
+const selectItems = [
+  { value: '2023' },
+  { value: '2022' },
+  { value: '2021' }
+];
+
+
+const searchTypes = {
+  order_number: { title: 'Номер заказа', placeholder: 'Поиск по номеру заказа' },
+  psid: { title: 'Номер фотосессии', placeholder: 'Поиск по номеру фотосессии' },
+  client_id: { title: 'Клиент ID', placeholder: 'Поиск по клиенту ID' },
+  phone: { title: 'Телефон', placeholder: 'Поиск по телефону' },
+  email: { title: 'Email', placeholder: 'Поиск по Email' },
+  payer: { title: 'Плательщик, ребенок', placeholder: 'Поиск по плательщику или ребенку' }
+};
+
+const handleBlur = () => {
+  console.log(selectedDate.value)
+};
+
+const handleSubmit = async () => {
+  isLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/lk/method/orders.getTest' + `?search_type=${searchType.value}&search_value=${searchQuery.value}&year=${selectedValue.value}`);
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки данных');
+    }
+    const data = await response.json();
+    items.value = data.response.data.orders;
+    console.log(items.value);
+  } catch (err: any) {
+    error.value = err.message;
+  } finally {
+    if(items.value.length > 0){
+      isLoading.value = false;
+    }
+  }
+};
+
+const fetchData = async () => {
+  isLoading.value = true;
+  try {
+  const response = await fetch('http://localhost:3001/lk/method/orders.getTest/');
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки данных');
+    }
+    const data = await response.json();
+    items.value = data.response.data.orders;
+    console.log(items.value);
+  } catch (err: any) {
+    error.value = err.message;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const updateIdFromUrl = () => {
+  const url = window.location.href;
+  const urlObj = new URL(url);
+  const pathname = urlObj.pathname;
+  const parts = pathname.split('/');
+  id.value = parts[parts.length - 1];
+  console.log(id.value);
+};
+
+const ShowButton = () => {
+  handleSubmit()
+};
+const ResetButton = () => {
+  selectedValue.value = '';
+  fetchData()
+};
+
+onMounted(fetchData);
+watchEffect(updateIdFromUrl);
+
+
+</script>
+<style lang="scss" scoped>
+@import 'src/shared/assets/styles/general/grid/_mixin.scss';
+
+.main_div{
+  background-color: white;
+  padding: 15px;
+}
+.block{
+  margin-top:15px;
+  margin-bottom:15px;
+}
+.toggle_block{
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+.toggle_block_element{
+  margin: 0 2.5px 5px;
+}
+.status-card{
+  padding: 15px;
+}
+.bttns_element{
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 0;
+}
+
+.my-element {
+  width: auto;
+
+  @include start-at(lg) {
+    display: none;
+  }
+
+  @include active-by(lg) {
+    width: 100%;
+  }
+}
+</style>
